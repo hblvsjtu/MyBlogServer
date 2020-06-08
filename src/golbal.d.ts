bbadc8fe54b77f3f1072a507a2915c2fc4e9ac19
hblvsjtu <hblvsjtu@163.com>
2020-06-08 21:03:44 +0800

    feat(InterceptManager): InterceptManager with promise API
    InterceptManager with promise API for normal Interceptor, and the next is routeInterceptor

--- src/golbal.d.ts
+++ src/golbal.d.ts
@@ -1,9 +1,18 @@
-interface StaticFilesOptions {
-    res: any;
-    path: string;
-    contentType?: string;
-    responseCode?: number;
-}
+type Method =
+    | 'get'
+    | 'GET'
+    | 'delete'
+    | 'DELETE'
+    | 'head'
+    | 'HEAD'
+    | 'options'
+    | 'OPTIONS'
+    | 'post'
+    | 'POST'
+    | 'put'
+    | 'PUT'
+    | 'patch'
+    | 'PATCH';
 
 interface StaticFilesOptions {
     res: any;
@@ -12,11 +21,46 @@ interface StaticFilesOptions {
     responseCode?: number;
 }
 
-type ResolveFn<T> = (data: T) => FileData | Promise<FileData> | void;
-
-type RejectFn = (data: Error | undefined) => any;
+interface StaticFilesOptions {
+    res: any;
+    path: string;
+    contentType?: string;
+    responseCode?: number;
+}
 
 interface FileData {
     next: string;
     message: string;
+}
+
+interface Next {
+    req: any;
+    res: any;
+}
+
+type ResolveFn<T> = (data: T) => Next | FileData | Promise<FileData | Next>;
+
+type RejectFn = (data: Error | undefined) => any;
+
+interface Interceptor<T> {
+    resolve: ResolveFn<T>;
+    reject?: RejectFn;
+}
+
+interface RouteInterceptor extends Interceptor<Next> {
+    url: string;
+    method: Method;
+}
+
+interface InterceptManager<T> {
+    use: (interceptor: Interceptor<T>) => number;
+    eject: (index: number) => void;
+}
+
+interface Router {
+    interceptorList: Array<Interceptor<any>>;
+    interceptManager: InterceptManager<any>;
+    execute: (next: Next) => Next | Promise<Next>;
+    get: (url: string, interceptor: Interceptor<Next>) => Router;
+    post: (url: string, interceptor: Interceptor<Next>) => Router;
 }
