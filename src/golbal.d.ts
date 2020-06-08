3a6df2f342b450d203528270eb703d4cb74c02e3
hblvsjtu <hblvsjtu@163.com>
2020-06-08 18:10:27 +0800

    feat(fileRead): fileRead method
    fileRead method with promise

--- src/golbal.d.ts
+++ src/golbal.d.ts
@@ -4,3 +4,19 @@ interface StaticFilesOptions {
     contentType?: string;
     responseCode?: number;
 }
+
+interface StaticFilesOptions {
+    res: any;
+    path: string;
+    contentType?: string;
+    responseCode?: number;
+}
+
+type ResolveFn<T> = (data: T) => FileData | Promise<FileData> | void;
+
+type RejectFn = (data: Error | undefined) => any;
+
+interface FileData {
+    next: string;
+    message: string;
+}
