766d60fe19933b3f8557bbdde03c51505e017a15
hblvsjtu <hblvsjtu@163.com>
2020-07-06 23:27:11 +0800

    fix(login token): check login token
    check login token

--- src/router/blogs.ts
+++ src/router/blogs.ts
@@ -2,12 +2,14 @@ import {SuccessModel, FailModel} from '../model/resModel';
 import {updateBlog, getList, getDetail, deleteBlog, createBlog} from '../controller/blogs';
 import checkProfile from './check';
 import {writeErrorLog} from '../utils/io';
+const {get} = require('lodash');
 
 // 获取博客列表
 export const getBlogListRouter = async ({req, res}: Next): Promise<Next> => {
     try {
         const {userId, token} = req.body;
-        await checkProfile(userId, token);
+        const cookieToken = get(req, 'cookie.token');
+        await checkProfile(userId, token, cookieToken);
         const list: SqlCheckResult = await getList(req.body as ListRequestParams);
         const result: SuccessModel = new SuccessModel(list);
         res.end(JSON.stringify(result));
@@ -26,7 +28,8 @@ export const getBlogListRouter = async ({req, res}: Next): Promise<Next> => {
 export const getBlogDetailRouter = async ({req, res}: Next): Promise<Next> => {
     try {
         const {userId, token, id} = req.body;
-        const profile: Profile = await checkProfile(userId, token);
+        const cookieToken = get(req, 'cookie.token');
+        await checkProfile(userId, token, cookieToken);
         const blog: Blog = (await getDetail(id))[0];
         const result: SuccessModel = new SuccessModel(blog);
         res.end(JSON.stringify(result));
@@ -45,7 +48,8 @@ export const getBlogDetailRouter = async ({req, res}: Next): Promise<Next> => {
 export const createBlogRouter = async ({req, res}: Next): Promise<Next> => {
     try {
         const {userId, token} = req.body;
-        await checkProfile(userId, token);
+        const cookieToken = get(req, 'cookie.token');
+        await checkProfile(userId, token, cookieToken);
         const id = (await createBlog(req.body as Blog)).insertId;
         const result: SuccessModel = new SuccessModel({id});
         // const data: Array<FileData> = await readFile(path.join(MODEL_DATA_DIR, 'a1.json'));
@@ -65,7 +69,8 @@ export const createBlogRouter = async ({req, res}: Next): Promise<Next> => {
 export const updateBlogRouter = async ({req, res}: Next): Promise<Next> => {
     try {
         const {userId, token, id, content} = req.body;
-        await checkProfile(userId, token);
+        const cookieToken = get(req, 'cookie.token');
+        await checkProfile(userId, token, cookieToken);
         if (!id) {
             throw new Error('id is not found!');
         }
@@ -87,7 +92,8 @@ export const updateBlogRouter = async ({req, res}: Next): Promise<Next> => {
 export const deleteBlogRouter = async ({req, res}: Next): Promise<Next> => {
     try {
         const {userId, token} = req.body;
-        await checkProfile(userId, token);
+        const cookieToken = get(req, 'cookie.token');
+        await checkProfile(userId, token, cookieToken);
         const affectedRows: number = (await deleteBlog(req.body.id)).affectedRows;
         const result: SuccessModel = new SuccessModel({affectedRows});
         res.end(JSON.stringify(result));
--- src/router/check.ts
+++ src/router/check.ts
@@ -1,12 +1,16 @@
 import {redisGet} from '../db/index';
 import _ from 'lodash';
 
-const checkProfile = async (userId: number, token: string): Promise<Profile> => {
+const checkProfile = async (
+    userId: number,
+    token: string,
+    cookieToken: string
+): Promise<Profile> => {
     if (!userId || !token) {
         throw new Error('请先登陆！');
     }
     const sessionToken: string = await redisGet(userId);
-    if (sessionToken !== token) {
+    if (sessionToken !== token || sessionToken !== cookieToken) {
         throw new Error('登陆信息已过期，请重新登陆！');
     }
     const profile = (await redisGet(sessionToken)) as Profile;
